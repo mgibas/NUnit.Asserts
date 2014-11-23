c2e1d8dd7662568a100e4daf1550889a9e667832
mgibas <gibasmaciej@gmail.com>
2014-11-23 19:58:21 +0100

    Microsoft.Owin.Security initial commit

---
+++ src/NUnit.Asserts/NUnit.Asserts.Microsoft.Owin.Security/ClaimsIdentityAssert.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+using System.Linq;
+using NUnit.Framework;
+
+namespace NUnit.Asserts.Microsoft.Owin.Security
+{
+    public class ClaimsIdentityAssert
+    {
+        public static void ContainsClaim(string type, string value, ClaimsIdentity identity)
+        {
+            var expectedClaim = new Claim(type, value);
+            var actualClaimsList = identity.Claims.Select(c => c.ToString());
+            var actualClaimsMsg = actualClaimsList.Aggregate((i, j) => i + "\n" + j);
+
+            var errorMessage = string.Format("Claim:\n{0}\nnot found between actual Claims:\n{1}", expectedClaim.ToString(), actualClaimsMsg);
+            CollectionAssert.Contains(actualClaimsList, expectedClaim.ToString(), errorMessage);
+        }
+    }
+}
