623c4542f05fa9c1b112995209f0a6c5e95e7bc9
unknown <m.gibas@programisci17.centrala.kaczmarski.pl>
2014-12-22 14:40:09 +0100

    Added expression version of CompareAssert

--- src/NUnit.Asserts/NUnit.Asserts.Compare/CompareAssert.cs
+++ src/NUnit.Asserts/NUnit.Asserts.Compare/CompareAssert.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using KellermanSoftware.CompareNetObjects;
 using NUnit.Framework;
 
@@ -12,11 +15,23 @@ namespace NUnit.Asserts.Compare
     /// <param name="expected"></param>
     /// <param name="actual"></param>
     /// <param name="propertiesToIgnore">Properties that should be ignored when comparing objects</param>
-    public static void AreEqual(object expected, object actual, params string[] propertiesToIgnore)
+    public static void AreEqual<T>(T expected, T actual, params string[] propertiesToIgnore)
     {
       var compareConfig = new ComparisonConfig { MembersToIgnore = new List<string>(propertiesToIgnore) };
       ComparisonResult compareResult = new CompareLogic(compareConfig).Compare(expected, actual);
       Assert.IsTrue(compareResult.AreEqual, "Compared objects are not equal, please inspect differences:\n\n{0}", compareResult.DifferencesString);
+    }
+
+    /// <summary>
+    /// Examine objects equality with types checking
+    /// </summary>
+    /// <param name="expected"></param>
+    /// <param name="actual"></param>
+    /// <param name="propertiesToIgnore">Properties that should be ignored when comparing objects</param>
+    public static void AreEqual<T>(T expected, T actual, params Expression<Func<T, object>>[] propertiesToIgnore)
+    {
+      var propertyNames = propertiesToIgnore.Select(PropertyName);
+      AreEqual(expected, actual, propertyNames.ToArray());
     }
 
     /// <summary>
@@ -34,6 +49,36 @@ namespace NUnit.Asserts.Compare
       };
       ComparisonResult compareResult = new CompareLogic(compareConfig).Compare(expected, actual);
       Assert.IsTrue(compareResult.AreEqual, "Compared objects are not equal, please inspect differences:\n\n{0}", compareResult.DifferencesString);
+    }
+
+    /// <summary>
+    /// Examine objects equality without types checking
+    /// </summary>
+    /// <param name="expected"></param>
+    /// <param name="actual"></param>
+    /// <param name="propertiesToIgnore">Properties that should be ignored when comparing objects</param>
+    public static void AreEquivalent<T>(T expected, object actual, params Expression<Func<T, object>>[] propertiesToIgnore)
+    {
+      var propertyNames = propertiesToIgnore.Select(PropertyName);
+      AreEquivalent(expected, actual, propertyNames.ToArray());
+    }
+
+    private static string PropertyName<TModel>(Expression<Func<TModel, object>> property)
+    {
+      var body = property.Body as MemberExpression;
+
+      if (body == null)
+      {
+        var ubody = (UnaryExpression)property.Body;
+        body = ubody.Operand as MemberExpression;
+      }
+
+      if (body == null)
+      {
+        throw new ArgumentException("Expression must be of type MemberExpression or UnaryExpression.");
+      }
+
+      return body.Member.Name;
     }
   }
 }
