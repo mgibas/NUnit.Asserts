d68d988ea6b355a3bc7411a13993b72d374171ab
unknown <m.gibas@programisci17.centrala.kaczmarski.pl>
2014-12-22 12:45:38 +0100

    Initial version of CompareAssert

---
+++ src/NUnit.Asserts/NUnit.Asserts.Compare/CompareAssert.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using KellermanSoftware.CompareNetObjects;
+using NUnit.Framework;
+
+namespace NUnit.Asserts.Compare
+{
+  public class CompareAssert
+  {
+    public static void Compare(object expected, object actual, params string[] propertiesToIgnore)
+    {
+      var compareConfig = new ComparisonConfig { MembersToIgnore = new List<string>(propertiesToIgnore) };
+      ComparisonResult compareResult = new CompareLogic(compareConfig).Compare(expected, actual);
+      Assert.IsTrue(compareResult.AreEqual, "Compared objects are not equal, please inspect differences:\n\n{0}", compareResult.DifferencesString);
+    }
+  }
+}
+
+
