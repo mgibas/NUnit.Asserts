c295cf088b126b1d82ff246e0b0570e963b45645
unknown <m.gibas@programisci17.centrala.kaczmarski.pl>
2014-12-22 13:23:57 +0100

    Refined interface

--- src/NUnit.Asserts/NUnit.Asserts.Compare/CompareAssert.cs
+++ src/NUnit.Asserts/NUnit.Asserts.Compare/CompareAssert.cs
@@ -6,9 +6,32 @@ namespace NUnit.Asserts.Compare
 {
   public class CompareAssert
   {
-    public static void Compare(object expected, object actual, params string[] propertiesToIgnore)
+    /// <summary>
+    /// Examine objects equality with types checking
+    /// </summary>
+    /// <param name="expected"></param>
+    /// <param name="actual"></param>
+    /// <param name="propertiesToIgnore">Properties that should be ignored when comparing objects</param>
+    public static void AreEqual(object expected, object actual, params string[] propertiesToIgnore)
     {
       var compareConfig = new ComparisonConfig { MembersToIgnore = new List<string>(propertiesToIgnore) };
+      ComparisonResult compareResult = new CompareLogic(compareConfig).Compare(expected, actual);
+      Assert.IsTrue(compareResult.AreEqual, "Compared objects are not equal, please inspect differences:\n\n{0}", compareResult.DifferencesString);
+    }
+
+    /// <summary>
+    /// Examine objects equality without types checking
+    /// </summary>
+    /// <param name="expected"></param>
+    /// <param name="actual"></param>
+    /// <param name="propertiesToIgnore">Properties that should be ignored when comparing objects</param>
+    public static void AreSimilar(object expected, object actual, params string[] propertiesToIgnore)
+    {
+      var compareConfig = new ComparisonConfig
+      {
+        MembersToIgnore = new List<string>(propertiesToIgnore),
+        IgnoreObjectTypes = true
+      };
       ComparisonResult compareResult = new CompareLogic(compareConfig).Compare(expected, actual);
       Assert.IsTrue(compareResult.AreEqual, "Compared objects are not equal, please inspect differences:\n\n{0}", compareResult.DifferencesString);
     }
